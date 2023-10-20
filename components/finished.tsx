aa02f5bc0691855d72cc8b8a0a6d09ec8a6bc77e
cakuang1 <cakuang1@berkeley.edu>
2023-10-19 18:13:13 -0700

    got color of difficulty to work:

--- components/finished.tsx
+++ components/finished.tsx
@@ -1,80 +1,84 @@
-
-
+import React from 'react';
 
 interface ProgressTrackerRow {
-    question: string;
-    difficulty: string;
-    topics: string[];
-    timeTaken: number; // You can use a numeric data type to represent time taken (e.g., minutes)
-    dateCompleted: Date;
-    codeOrNotes: string; // You may use a string to store code or notes
-  }
+  question: string;
+  difficulty: string;
+  topics: string[];
+  timeTaken: number;
+  dateCompleted: Date;
+  codeOrNotes: string;
+}
 
 
+function getColorClasses(difficulty : String) {
+    switch (difficulty) {
+      case 'Easy':
+        return 'bg-green-100 text-green-800 border-green-200';
+      case 'Medium':
+        return 'bg-yellow-100 text-yellow-800 border-yellow-200';
+      case 'Hard':
+        return 'bg-red-100 text-red-800 border-red-200';
+      default:
+        return ''; 
+    }
+}
 
 
 const ProgressTracker = ({ progressList }: { progressList: ProgressTrackerRow[] }) => {
-    return (
-
-        
-      <div className="w-3/5 mx-auto">
-        <h1 className="text-center text-2xl font-bold">Problems</h1>
-        <h1 className="font-semibold text-gray-500">Completed</h1>
-        <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
-    <thead className="bg-gray-50">
-        <tr>
+  return (
+    <div className="w-3/5 mx-auto">
+      <h1 className="text-center text-2xl font-bold">Problems</h1>
+      <h1 className="font-semibold text-gray-500">Completed</h1>
+      <table className="min-w-full divide-y divide-gray-200 overflow-x-auto">
+        <thead className="bg-gray-50">
+          <tr>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Solved
+         </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Question
+              Question
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Difficulty
+              Difficulty
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Topics
+              Topics
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Time Taken
+              Time Taken
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Date Completed
+              Date Completed
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Code/Notes
+              Code/Notes
             </th>
-        </tr>
-    </thead>
-    <tbody className="bg-white divide-y divide-gray-200">
-        <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
-                <div className="text-sm text-gray-500">Optimization</div>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    Active
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {progressList.map((row, index) => (
+            <tr key={index}>
+              <td className="px-6 py-4 whitespace-nowrap">{row.question}</td>
+              <td className={`px-6 py-4 whitespace-nowrap `}>
+                <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${getColorClasses(row.difficulty)} text-green-800`}>
+                    {row.difficulty}
                 </span>
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    Active
-                </span>
-                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    Active
-                </span>
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                jane.cooper@example.com
-            </td>
-        </tr>
 
-    </tbody>
-</table>  
-      </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">{row.topics.join(', ')}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{row.timeTaken} minutes</td>
+              <td className="px-6 py-4 whitespace-nowrap">{row.dateCompleted.toDateString()}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <pre>
+                  <code>{row.codeOrNotes}</code>
+                </pre>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    )
-}
-  
-export default ProgressTracker
+export default ProgressTracker;
--- pages/auth/problems.tsx
+++ pages/auth/problems.tsx
@@ -1,13 +1,30 @@
 import ProgressTracker from "@/components/finished"
 
-
+const progressData = [
+  {
+    question: 'Problem 1',
+    difficulty: 'Medium',
+    topics: ['Array', 'Dynamic Programming'],
+    timeTaken: 45,
+    dateCompleted: new Date('2023-01-15'),
+    codeOrNotes: 'Code and notes for problem 1...',
+  },
+  {
+    question: 'Problem 2',
+    difficulty: 'Easy',
+    topics: ['String', 'Hashing'],
+    timeTaken: 30,
+    dateCompleted: new Date('2023-01-20'),
+    codeOrNotes: 'Code and notes for problem 2...',
+  },
+  // Add more ProgressTrackerRow objects as needed
+];
 
 
 export default function Problems() {
   return (
     <div>
-      <ProgressTracker/>
-
+      <ProgressTracker progressList={progressData}/>
     </div>
   )
 }
